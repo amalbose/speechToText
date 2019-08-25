acd009e7c90ffeca0329ae47c5cfddbb85dd6b0c
Amal Bose <axatrikx@gmail.com>
2019-08-25 12:41:17 +0530

    speak from query param

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
 var exec = require("child_process").exec;
-var cmd = "/home/pi/speech.sh Hello";
+var cmd = "/home/pi/speech.sh ";
 
 app.get("/speak", (req, res, next) => {
+  var text = req.query.t;
+  cmd = cmd + text;
   exec(cmd, function(error, stdout, stderr) {
-    res.json(error || stdout || stderr);
+    res.json(req);
   });
 });
 
